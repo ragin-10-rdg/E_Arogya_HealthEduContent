1e074d61a67b2d75f52489903c89f1eefe389dd5
ragin-10-rdg <zeroremorsenepal@gmail.com>
2025-08-15 09:34:46 +0545

    first commit

---
+++ E-Arogya-Backend/earogya_backend/urls.py
@@ -0,0 +1,19 @@
+"""
+URL configuration for earogya_backend project.
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.generic import RedirectView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('health_content.urls')),
+    path('favicon.ico', RedirectView.as_view(url=settings.STATIC_URL + 'favicon.ico')),
+]
+
+# Serve media files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
